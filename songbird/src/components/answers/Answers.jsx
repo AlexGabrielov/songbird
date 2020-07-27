22aa76566b703f5afd9815c72289af6b3b9c58f2
Alex <alex.gabriel@mail.ru>
2020-07-27 16:26:02 +0300

    feat: add basic answers jsx

--- songbird/src/components/answers/Answers.jsx
+++ songbird/src/components/answers/Answers.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
-const Answers = (props) => {}
+const Answers = (props) => {
+  return (
+    <div className="answers-wrapper">
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+    </ul>
+    </div>
+  )
+}
 
 export default Answers;
