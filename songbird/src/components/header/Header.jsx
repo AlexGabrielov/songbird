3212ab4438a029002b63fa67ee87cc22acf8e14f
Alex <alex.gabriel@mail.ru>
2020-07-27 16:13:55 +0300

    feat: add basic header jsx

--- songbird/src/components/header/Header.jsx
+++ songbird/src/components/header/Header.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
-const Header = (props) => {}
+const Header = (props) => {
+  return (
+    <header className="header-wrapper">
+    <div className="logo-and-score-wrapper">
+      <div className="logo"></div>
+      <div className='score'></div>
+    </div>
+    <div className="category-wrapper">
+      <div className="category">Разминка</div>
+      <div className="category">Воробьиные</div>
+      <div className="category">Лесные птицы</div>
+      <div className="category">Певчие птицы</div>
+      <div className="category">Хищные птицы</div>
+      <div className="category">Морские птицы</div>
+    </div>
+    </header>
+  )
+}
 
 export default Header;
