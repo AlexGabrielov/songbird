482d2f9e5fa92f5fb8676e4c4964812506014a53
Alex <alex.gabriel@mail.ru>
2020-07-27 14:59:40 +0300

    feat: add birds info and logo

--- songbird/src/App.js
+++ songbird/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import './App.css';
+import Answers from '../src/components/answers/Answers';
+import BirdInfo from '../src/components/bird-info/BirdInfo';
+import CurrentQuestion from '../src/components/current-question/CurrentQuestion';
+import Header from '../src/components/header/Header';
+import birdsData from '../src/birds';
 
 function App() {
-  return <div>test</div>
+  return (
+    <div className="app-container">
+      <Header />
+      <CurrentQuestion />
+      <Answers />
+      <BirdInfo />
+    </div>
+  );
 }
 
 export default App;
