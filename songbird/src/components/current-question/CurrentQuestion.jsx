4fe52c3565fc3c78e437a25ce0bef27b8fd55145
Alex <alex.gabriel@mail.ru>
2020-07-27 16:18:28 +0300

    feat: add basic current question jsx

--- songbird/src/components/current-question/CurrentQuestion.jsx
+++ songbird/src/components/current-question/CurrentQuestion.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const CurrentQuestion = (props) => {}
+const CurrentQuestion = (props) => {
+  return (
+    <div className='question-wrapper'>
+      <div className='bird-name-container'></div>
+      <div className='bird-image-container'></div>
+      <div className='audio-player-container'></div>
+    </div>
+  )
+}
 
 export default CurrentQuestion;
