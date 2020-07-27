2ced39cb2670b3835f5b2ff001c39a2bd89309e3
Alex <alex.gabriel@mail.ru>
2020-07-27 16:32:47 +0300

    feat: add basic bird info jsx

--- songbird/src/components/bird-info/BirdInfo.jsx
+++ songbird/src/components/bird-info/BirdInfo.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
-const BirdInfo = (props) => {}
+const BirdInfo = (props) => {
+  return (
+    <div className='bird-info-wrapper'>
+      <div className='bird-image-container-info'>image</div>
+      <div className='bird-name-and-player-wrapper'>
+        <div className='bird-name-container-info'>ястреб</div>
+        <div className="bird-latin-name-container-info">ястреб латин</div>
+        <div className='audio-player-container-info'>плеер</div>
+      </div>
+      <div className="bird-description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Repellat, sed! Non qui assumenda delectus consectetur aperiam explicabo ad praesentium illum ipsam
+        facere vel consequuntur labore, ab asperiores neque! Blanditiis, aspernatur!
+       </div>
+    </div>
+  )
+}
 
 export default BirdInfo;
