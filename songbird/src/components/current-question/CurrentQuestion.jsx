42659eb862354132e8df91999ce91e36d9b6faea
Alex <alex.gabriel@mail.ru>
2020-08-04 14:58:10 +0300

    feat: remove strict and change require to import

--- songbird/src/components/current-question/CurrentQuestion.jsx
+++ songbird/src/components/current-question/CurrentQuestion.jsx
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
 import birdsData from '../../birds';
+import defaultBird from '../../unknown-bird.jpg';
 
 const CurrentQuestion = ({categoryIndex, currentBirdIndex}) => {
   return (
     <div className='question-wrapper'>
-      <img src='../../unknown-bird.jpg' alt="bird" className='bird-img-question'/>
+      <img src={defaultBird} alt="bird" className='bird-img-question'/>
       <div className="group-name-and-player">
         <div className='bird-name-container'>Ястреб</div>
         <div className='audio-player-container'>
